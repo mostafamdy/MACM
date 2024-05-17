eac9cb0ee50d827066c826f26cb592cef021d680
mostafamdy <76405917+mostafamdy@users.noreply.github.com>
2024-05-17 20:00:59 +0300

    add gemma model
    updated inference to work with gemma code from transformers

--- utils/gpt_robots.py
+++ utils/gpt_robots.py
@@ -1,163 +1,77 @@
 import os
-from openai import OpenAI
-os.environ["OPENAI_API_KEY"] = "" # Enter your OpenAi Key
-client = OpenAI()
+# from openai import OpenAI
+# os.environ["OPENAI_API_KEY"] = "" # Enter your OpenAi Key
+# client = OpenAI()
+import gc
+import torch
+from transformers import BitsAndBytesConfig, AutoModelForCausalLM, GemmaTokenizer
 
-   
-def generate_from_thinker(prompts, max_tokens, model="gpt-4-1106-preview", temperature=0.7, n=1):
-    assistant = client.beta.assistants.create(
-        model=model,
-        instructions="""You are a thinker. I need you to help me think about some problems.
-        You need to provide me the answer based on the format of the example.""",
-        name="Thinker",
-        tools=[{"type": "code_interpreter"}],
+# torch.backends.cuda.enable_mem_efficient_sdp(False)
+
+tokenizer = GemmaTokenizer.from_pretrained("/kaggle/input/codegemma/transformers/7b-it/1")
+
+_model = AutoModelForCausalLM.from_pretrained(
+    "/kaggle/input/codegemma/transformers/7b-it/1",
+    device_map="auto",
+    torch_dtype="auto",
+    quantization_config=BitsAndBytesConfig(
+        load_in_4bit=True,  # Loading weights in 4-bit format
+        bnb_4bit_quant_type="nf4",  # Using non-linear quantization with 4 bits
+        bnb_4bit_compute_dtype=torch.bfloat16,  # Using bfloat16 for computation
+        bnb_4bit_use_double_quant=True  # Using double quantization
     )
+)
 
-    thread = client.beta.threads.create()
-    for i in range(len(prompts)):   
-        message = prompts[i]["content"]
-
-        thread_message = client.beta.threads.messages.create(
-            thread_id=thread.id,
-            role="user",
-            content=message,
-        ) 
-        
-        run = client.beta.threads.runs.create(
-            thread_id=thread.id,
-            assistant_id=assistant.id,
-        )      
-        while run.status in ["queued", "in_progress"]:
-            keep_retrieving_run = client.beta.threads.runs.retrieve(
-                thread_id=thread.id,
-                run_id=run.id
-            )
-            
-            if keep_retrieving_run.status == "completed":
-                # print("\n")
-
-                all_messages = client.beta.threads.messages.list(
-                    thread_id=thread.id
-                )
-                # print(f"all_messages:\n{all_messages}")
-                
-                try:
-                    Assistant_response = all_messages.data[0].content[0].text.value
-                except Exception as e:
-                    print(f"An error occurred: {e}") # Avoid the image outputs
-                    Assistant_response = "I need to rethink this problem."
-                break
-
-            elif keep_retrieving_run.status == "queued" or keep_retrieving_run.status == "in_progress":
-                pass
-            else:
-                print(f"Run status: {keep_retrieving_run.status}")
-                break
-    return Assistant_response  
-        
+def generate_from_thinker(prompts, max_tokens, model="gpt-4-1106-preview", temperature=0.7, n=1):
+    instructions = """You are a thinker. I need you to help me think about some problems.
+        You need to provide me the answer based on the format of the example."""
+    message=instructions
+    for i in range(len(prompts)): 
+        message += prompts[i]["content"]
+       
+    tokens = tokenizer(message, return_tensors='pt').to(model.device)
+   
+    outputs = _model.generate(
+        **tokens,
+        max_new_tokens=max_tokens,
+        do_sample=True,
+        temperature=temperature,
+     )
+    context = tokenizer.decode(outputs[0][1:-1])
+    return context[len(message):]    
 
 
 def generate_from_judge(prompts, max_tokens, model="gpt-4-1106-preview", temperature=0.7, n=1):
-
-    assistant = client.beta.assistants.create(
-        model=model,
-        instructions="""You're a judge. I need you to make judgments on some statements.""",
-        name="Judge",
-        tools=[{"type": "code_interpreter"}],
-    )
-
-    thread = client.beta.threads.create()
-    for i in range(len(prompts)):   
-        message = prompts[i]["content"]
-
-        thread_message = client.beta.threads.messages.create(
-            thread_id=thread.id,
-            role="user",
-            content=message,
-        ) 
-        
-        run = client.beta.threads.runs.create(
-            thread_id=thread.id,
-            assistant_id=assistant.id,
-        )      
-        while run.status in ["queued", "in_progress"]:
-            keep_retrieving_run = client.beta.threads.runs.retrieve(
-                thread_id=thread.id,
-                run_id=run.id
-            )
-            
-            if keep_retrieving_run.status == "completed":
-                # print("\n")
-
-                all_messages = client.beta.threads.messages.list(
-                    thread_id=thread.id
-                )
-                # print(f"all_messages:\n{all_messages}")
-                
-                try:
-                    Assistant_response = all_messages.data[0].content[0].text.value
-                except Exception as e:
-                    print(f"An error occurred: {e}") # Avoid the image outputs
-                    Assistant_response = "False"
-                break
-
-            elif keep_retrieving_run.status == "queued" or keep_retrieving_run.status == "in_progress":
-                pass
-            else:
-                print(f"Run status: {keep_retrieving_run.status}")
-                break
-    return Assistant_response
+    instructions = """You're a judge. I need you to make judgments on some statements."""
+    message=instructions
+    for i in range(len(prompts)): 
+        message += prompts[i]["content"]
+       
+    tokens = tokenizer(message, return_tensors='pt').to(model.device)
+    outputs = _model.generate(
+        **tokens,
+        max_new_tokens=max_tokens,
+        do_sample=True,
+        temperature=temperature,
+     )
+    context = tokenizer.decode(outputs[0][1:-1])
+    return context[len(message):]   
 
 
 
 def generate_from_excutor(prompts, max_tokens, model="gpt-4-1106-preview", temperature=0.7, n=1):
-    assistant = client.beta.assistants.create(
-        model=model,
-        instructions="""You're an excutor. I need you to calculate the final result based on some conditions and steps.
-        You need to provide me the answer based on the format of the examples.""",
-        name="Excutor",
-        tools=[{"type": "code_interpreter"}],
-    )
-
-    thread = client.beta.threads.create()
-    for i in range(len(prompts)):   
-        message = prompts[i]["content"]
-
-        thread_message = client.beta.threads.messages.create(
-            thread_id=thread.id,
-            role="user",
-            content=message,
-        ) 
-        
-        run = client.beta.threads.runs.create(
-            thread_id=thread.id,
-            assistant_id=assistant.id,
-        )      
-        while run.status in ["queued", "in_progress"]:
-            keep_retrieving_run = client.beta.threads.runs.retrieve(
-                thread_id=thread.id,
-                run_id=run.id
-            )
-            
-            if keep_retrieving_run.status == "completed":
-                # print("\n")
-
-                all_messages = client.beta.threads.messages.list(
-                    thread_id=thread.id
-                )
-                # print(f"all_messages:\n{all_messages}")
-                
-                try:
-                    Assistant_response = all_messages.data[0].content[0].text.value
-                except Exception as e:
-                    print(f"An error occurred: {e}") # Avoid the image outputs
-                    Assistant_response = "False"
-                break
-
-            elif keep_retrieving_run.status == "queued" or keep_retrieving_run.status == "in_progress":
-                pass
-            else:
-                print(f"Run status: {keep_retrieving_run.status}")
-                break
-    return Assistant_response
+    instructions="""You're an excutor. I need you to calculate the final result based on some conditions and steps.
+    You need to provide me the answer based on the format of the examples."""
+    message=instructions
+    for i in range(len(prompts)): 
+        message += prompts[i]["content"]
+    tokens = tokenizer(message, return_tensors='pt').to(model.device)
+    outputs = _model.generate(
+        **tokens,
+        max_new_tokens=max_tokens,
+        do_sample=True,
+        temperature=temperature,
+     )
+    context = tokenizer.decode(outputs[0][1:-1])
+    return context[len(message):]   
 
